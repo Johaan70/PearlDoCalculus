1125985a47a82aec9efa322e179843eb6de276d9
Johan <johan@ellingsoya>
2026-08-20 14:26:04 +0000

    verifisert: tsum_assignmentSplit_fixed_left virker med restrict-form

--- Probe34.lean
+++
@@ -1,8 +0,0 @@
-import PearlDoCalculus
-open PearlDoCalculus DAG DAG.CausalModel Classical
-example {V : Type} [DecidableEq V] [Fintype V] {α : V → Type}
-    (X Y Z : Finset V) (hXY : Disjoint X Y) (hXYZ : Disjoint (X ∪ Y) Z)
-    (c : (Assignment (α := α) X × Assignment (α := α) Y) × Assignment (α := α) Z) :
-    ((assignmentSplit3 X Y Z hXY hXYZ).symm c).restrict (subset_union3_right X Y Z) = c.2 := by
-  funext v
-  simp [assignmentSplit3, assignmentSplit, Assignment.restrict, Equiv.Finset.union]
